83a43a16f36da8df13b7c7bd2955986b5e593af4
Jakub Jirous <info@jakubjirous.cz>
2023-07-08 12:12:51 -0700

    [config] add fluid typography

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -13,8 +13,8 @@ module.exports = {
 				inter: ["InterVariable", "Inter", ...defaultTheme.fontFamily.sans],
 			},
 			colors: {
-				darkGrey: "#151515", // heading 1
-				riceFlower: "#EFFFE2", // heading 2
+				"dark-grey": "#151515", // heading 1
+				"rice-flower": "#EFFFE2", // heading 2
 
 				silver: "#C7C7C7", // body1 / gradient !!!
 
@@ -22,36 +22,50 @@ module.exports = {
 
 				boulder: "#757575", // small
 
-				codGray: "#1A1A1A", // screen bg
+				"cod-gray": "#1A1A1A", // screen bg
 				malibu: "#44DBFF", // code
 
-				hollywoodCerise: "#FF069C" // footer heart
+				"hollywood-cerise": "#FF069C", // footer heart
 			},
 			fontSize: {
 				// heading 1
+				"clamp-h1": "clamp(3.5rem, 0.4654rem + 9.8089vw, 8.3125rem)",
 				56: ["3.5rem", { lineHeight: "1.1525" }],
 				82: ["5.125rem", { lineHeight: "1.15976" }],
-				133: ["8.3125rem", { lineHeight: "1.15973", }],
+				133: ["8.3125rem", { lineHeight: "1.15973" }],
 
-				// heading 1
-				32: ["3.5rem", { lineHeight: "1" }],
-				48: ["3rem", { lineHeight: "1" }],
-				76: ["4.75rem", { lineHeight: "1", }],
+				// heading 2
+				"clamp-h2": "clamp(3rem, 1.8965rem + 3.5669vw, 4.75rem)",
+				32: ["3rem", { lineHeight: "1" }],
+				48: ["3.5rem", { lineHeight: "1" }],
+				76: ["4.75rem", { lineHeight: "1" }],
 
 				// body1
-				26: ["1.625rem", { lineHeight: "2.635" }], // 162%
+				"clamp-body1": "clamp(1.375rem, 1.2174rem + 0.5096vw, 1.625rem)",
+				22: ["1.375rem", { lineHeight: "162%" }],
+				// 24:
+				26: ["1.625rem", { lineHeight: "162%" }],
 
 				// body2
-				24: ["1.5rem", { lineHeight: "2.43" }], // 162%
+				"clamp-body2": "clamp(1.25rem, 1.0924rem + 0.5096vw, 1.5rem)",
+				20: ["1.25rem", { lineHeight: "162%" }],
+				// 22:
+				24: ["1.5rem", { lineHeight: "162%" }],
 
 				// small
-				18: ["1.125rem", { lineHeight: "2.195" }], // 195.5%
+				"clamp-small": "clamp(1rem, 0.9212rem + 0.2548vw, 1.125rem)",
+				16 : ["1rem", { lineHeight: "195.5%" }],
+				// 18:
+				18: ["1.125rem", { lineHeight: "195.5%" }],
 
 				// code
 				8: ["0.502rem", { lineHeight: "2.195" }],
 
 				// footer ascii
-				54: ["3.375rem", { lineHeight: "5.4675" }], // 162%
+				"clamp-ascii": "clamp(1.6875rem, 0.6234rem + 3.4395vw, 3.375rem)",
+				27: ["1.6875rem", { lineHeight: "162%" }],
+				36: ["2.25rem", { lineHeight: "162%" }],
+				54: ["3.375rem", { lineHeight: "162%" }],
 			},
 		},
 	},
