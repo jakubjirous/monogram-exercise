4e0e21fe3e328990fef9a70c0fb1687b6066df16
Jakub Jirous <info@jakubjirous.cz>
2023-07-08 10:19:47 -0700

    [config] repo init and basic config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
-  integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-    mdx(),
-    sitemap(),
-  ],
+	site: "https://monogram-exercise-jakubjirous.vercel.app/",
+	integrations: [
+		tailwind(),
+		image({
+			serviceEntryPoint: "@astrojs/image/sharp",
+		}),
+	],
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,58 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["InterVariable", "Inter", ...defaultTheme.fontFamily.sans],
-      },
-    },
-  },
-  plugins: [require("@tailwindcss/typography")],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	theme: {
+		screens: {
+			tablet: "768px",
+			desktop: "1280px",
+		},
+		extend: {
+			fontFamily: {
+				inter: ["InterVariable", "Inter", ...defaultTheme.fontFamily.sans],
+			},
+			colors: {
+				darkGrey: "#151515", // heading 1
+				riceFlower: "#EFFFE2", // heading 2
+
+				silver: "#C7C7C7", // body1 / gradient !!!
+
+				tundora: "#414141", // body2
+
+				boulder: "#757575", // small
+
+				codGray: "#1A1A1A", // screen bg
+				malibu: "#44DBFF", // code
+
+				hollywoodCerise: "#FF069C" // footer heart
+			},
+			fontSize: {
+				// heading 1
+				56: ["3.5rem", { lineHeight: "1.1525" }],
+				82: ["5.125rem", { lineHeight: "1.15976" }],
+				133: ["8.3125rem", { lineHeight: "1.15973", }],
+
+				// heading 1
+				32: ["3.5rem", { lineHeight: "1" }],
+				48: ["3rem", { lineHeight: "1" }],
+				76: ["4.75rem", { lineHeight: "1", }],
+
+				// body1
+				26: ["1.625rem", { lineHeight: "2.635" }], // 162%
+
+				// body2
+				24: ["1.5rem", { lineHeight: "2.43" }], // 162%
+
+				// small
+				18: ["1.125rem", { lineHeight: "2.195" }], // 195.5%
+
+				// code
+				8: ["0.502rem", { lineHeight: "2.195" }],
+
+				// footer ascii
+				54: ["3.375rem", { lineHeight: "5.4675" }], // 162%
+			},
+		},
+	},
 };
