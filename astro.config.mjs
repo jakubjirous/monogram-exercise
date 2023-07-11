6f0137d463ba13fcaf9743af616ea5b022a0701b
Jakub Jirous <info@jakubjirous.cz>
2023-07-11 15:10:21 -0700

    [config] code cleanup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
-import vercel from '@astrojs/vercel/static';
 
 export default defineConfig({
 	site: "https://monogram-exercise-jakubjirous.vercel.app/",
