3a11450d0f7349fbf0e0c1a1843f6bc9a195caa3
Jakub Jirous <info@jakubjirous.cz>
2023-07-11 20:08:50 -0700

    [config] enable vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,24 +2,26 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import compressor from "astro-compressor";
+import vercel from "@astrojs/vercel/static";
 
+// https://astro.build/config
 export default defineConfig({
-	site: "https://monogram-exercise-jakubjirous.vercel.app/",
-	integrations: [
-		tailwind(),
-		image({
-			serviceEntryPoint: "@astrojs/image/sharp",
-		}),
-		compressor({
-			gzip: false,
-			brotli: true,
-			fileExtensions: [".html", ".js", ".css", ".svg", ".png", ".webp"],
-		}),
-	],
-	experimental: {
-		assets: true,
-	},
-	build: {
-		split: true,
-	},
+  site: "https://monogram-exercise-jakubjirous.vercel.app/",
+  integrations: [tailwind(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), compressor({
+    gzip: false,
+    brotli: true,
+    fileExtensions: [".html", ".js", ".css", ".svg", ".png", ".webp"]
+  })],
+  experimental: {
+    assets: true
+  },
+  output: "server",
+  adapter: vercel({
+    imageService: true,
+  }),
+  build: {
+    split: true
+  },
 });
